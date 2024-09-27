db40cc90ca8bc98a67103a2292bcf4e5ea8b64a0
pavielsaldana <124369853+pavielsaldana@users.noreply.github.com>
2024-09-27 14:04:00 -0500

    files

--- streamlit_scripts/data_cleaning_scripts/streamlit_ai_title_cleaning.py
+++ streamlit_scripts/data_cleaning_scripts/streamlit_ai_title_cleaning.py
@@ -5,6 +5,9 @@ import streamlit as st
 import time
 import uuid
 
+import os
+import sys
+sys.path.append(os.path.abspath('../streamlit_scripts/streamlit_utils'))
 from streamlit_scripts.streamlit_utils.py import base_request_url, generate_task_id, key_dict, OPENAI_API_KEY
 
 
